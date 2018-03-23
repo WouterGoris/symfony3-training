579c3db6242c2782e06a8a2dae118be5dc9da74c
unknown <woutergorisit@gmail.com>
2018-03-23 15:51:45 +0100

    added serialize and deserialze logic to user

--- project/src/AppBundle/Tests/Entity/User.php
+++ project/src/AppBundle/Tests/Entity/User.php
@@ -3,7 +3,7 @@
 namespace AppBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
-use Symfony\Component\Securty\Core\User\UserInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
  * User
@@ -26,6 +26,7 @@ class User implements UserInterface, \Serializable
 
      /**
       * @var string
+
       * @ORM\Column(name="username", type="string", length=255, unique=true)
       */
 
@@ -33,15 +34,95 @@ class User implements UserInterface, \Serializable
 
       /**
        * @var string
+       * 
        * @ORM\Column(name="password", type="string", length=64)
        */
 
-       private $password;
+        private $password;
 
-       /**
+        /**
         * @var string
+
         *@ORM\Column(name="email", type="string", length=255)
         */
 
         private $email;
+
+        /**
+         * Get id
+         * @return int
+         */
+
+        public function getId()
+        {
+            return $this->id;
+        }
+
+        /**
+         * Set username
+         * 
+         * @param string $username
+         * 
+         * @return user
+         */
+
+         public function setUsername($username)
+         {
+             $this->username = $username;
+
+             return $this;
+         }
+
+         public function getUsername()
+         {
+             return $this->username;
+         }
+
+         /**
+          * lol
+          */
+
+          public function getPassword()
+          {
+                return $this->password;
+          }
+
+          public function setPassword($password)
+          {
+              $this->password = $password;
+              return $this;
+          }
+
+          public function getEmail()
+          {
+              return $this->email;
+          }
+
+          public function setEmail($email)
+          { 
+              $this->email = $email;
+              return $this;
+          }
+
+
+
+
+
+        public function serialize()
+        {
+            return serialize ([
+                $this->id,
+                $this->username,
+                $this->password,
+            ]);
+        }
+
+        public function unserialize($serialized)
+        {
+            list(
+                $this->id,
+                $this->username,
+                $this->password,
+            ) = unserialize($serialized);
+        }
 }
